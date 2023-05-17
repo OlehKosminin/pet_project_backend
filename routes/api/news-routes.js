d3f24a3de82ea3b64aff0143b5cf1c2b9f2dbf29
Olehys <arzamazova13@gmail.com>
2023-05-17 09:31:44 +0300

    fsdfs

--- routes/api/news-routes.js
+++ routes/api/news-routes.js
@@ -1,12 +1,9 @@
 const express = require("express");
 
-const { authenticate } = require("../../middlewares");
-
 const ctrl = require("../../controllers/news-controllers");
 
 const router = express.Router();
 
-router.get("/", authenticate, ctrl.getNews);
-// router.post("/", authenticate, ctrl.getNews);
+router.get("/", ctrl.getNews);
 
 module.exports = router;
