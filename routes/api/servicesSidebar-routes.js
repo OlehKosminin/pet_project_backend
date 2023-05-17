351d688cc0df320d60b9656b402eacdf5a4dff49
Olehys <arzamazova13@gmail.com>
2023-05-17 09:07:16 +0300

    add news,partner controlers

--- routes/api/servicesSidebar-routes.js
+++ routes/api/servicesSidebar-routes.js
@@ -1,28 +1,11 @@
 const express = require("express");
 
-const { validateBody } = require("../../utils");
-
 const { authenticate } = require("../../middlewares");
 
-const { schemas } = require("../../models/user");
-
-const ctrl = require("../../controllers/auth-controllers");
+const ctrl = require("../../controllers/servicesSidebar-controllers");
 
 const router = express.Router();
 
-router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
-
-router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
-
-router.get("/current", authenticate, ctrl.getCurrent);
-
-router.post("/logout", authenticate, ctrl.logout);
-
-router.patch(
-  "/users",
-  authenticate,
-  validateBody(schemas.updateSubscriptionSchema),
-  ctrl.updateSubscription
-);
+router.get("/", authenticate, ctrl.getPartnerInfo);
 
 module.exports = router;
