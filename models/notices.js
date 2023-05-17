7dc2acab038d65fcf60f298d6cf864881f12e604
Fredy228 <smagrovich58@gmail.com>
2023-05-17 14:59:38 +0300

    add routes and fix

--- models/notices.js
+++ models/notices.js
@@ -1,4 +1,5 @@
 const { model, Schema } = require("mongoose");
+const Joi = require("joi");
 
 const noticesSchema = new Schema({
   title: {
@@ -33,7 +34,11 @@ const noticesSchema = new Schema({
   },
   photoUrl: {
     type: String,
-    default: null,
+    required: [true, "The photo field must be filled"],
+  },
+  public_id: {
+    type: String,
+    required: [true, "The public_id field must be filled"],
   },
   location: {
     type: String,
@@ -55,4 +60,25 @@ const noticesSchema = new Schema({
 
 const Notices = model("notices", noticesSchema);
 
-module.exports = Notices;
+const noticesCreateValidator = (data) =>
+  Joi.object()
+    .keys({
+      title: Joi.string().min(10).max(80).required(),
+      name: Joi.string().min(2).max(20).required(),
+      birthday: Joi.string().required(), // уточняеться...
+      breed: Joi.string().min(2).max(20).required(),
+      sex: Joi.string().valid("male", "female", "unknown").default("unknown"),
+      category: Joi.string()
+        .valid("sell", "in good hands", "lost/found")
+        .default("sell"),
+      comments: Joi.string().min(20).max(500),
+      location: Joi.string().min(2).max(20),
+      price: Joi.number().default(null),
+    })
+    .options({ stripUnknown: true })
+    .validate(data);
+
+module.exports = {
+  Notices,
+  noticesCreateValidator,
+};
