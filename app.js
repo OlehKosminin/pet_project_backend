b49b2e3c9e5486f2214e3bcd327d45a0a2f79152
Fredy228 <smagrovich58@gmail.com>
2023-05-18 16:37:29 +0300

    add-swagger-notices

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 const authRouter = require("./routes/api/auth-routes");
 const petsRouter = require("./routes/api/pets-routes");
 const notiesRouter = require("./routes/api/noties-routes");
@@ -13,6 +16,7 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
